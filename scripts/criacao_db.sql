1c9c5e4f5091a6b0b88a35df45e381d06df4e350
Melissa Neves <melissa.neves@sptech.school>
2024-04-14 14:00:50 -0300

    @melissaSBNeves

--- scripts/criacao_db.sql
+++ scripts/criacao_db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE db_naregua;
 USE db_naregua;
 
 CREATE TABLE IF NOT EXISTS endereco (
-  id_endereco INT NOT NULL auto_increment,
+  id_endereco INT NOT NULL AUTO_INCREMENT,
   cep CHAR(9) NULL,
   logradouro VARCHAR(250) NULL,
   numero INT NULL,
@@ -13,47 +13,53 @@ CREATE TABLE IF NOT EXISTS endereco (
 );
   
 CREATE TABLE IF NOT EXISTS especialidade (
-  id_especialidade INT NOT NULL,
+  id_especialidade INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(180) NULL,
   PRIMARY KEY (id_especialidade)
 );
 
 CREATE TABLE IF NOT EXISTS avaliacao (
-  id_avaliacao INT NOT NULL,
+  id_avaliacao INT NOT NULL AUTO_INCREMENT,
   resultado_avaliacao DECIMAL(3,2) NULL,
   constraint check_avaliacao check (resultado_avaliacao <= 5 AND resultado_avaliacao >= 0),
   PRIMARY KEY (id_avaliacao)
 );
 
-CREATE TABLE IF NOT EXISTS barbearia (
-  id_barbearia INT NOT NULL AUTO_INCREMENT,
-  nome_negocio VARCHAR(100) NOT NULL,
-  email_negocio VARCHAR(100),
-  celular_negocio VARCHAR(15),
-  cnpj VARCHAR(18),
-  cpf VARCHAR(14),
-  img_perfil BLOB,
-  barbearia_fk_endereco INT,
-  PRIMARY KEY (id_barbearia),
-  CONSTRAINT fk_endereco_barbearia FOREIGN KEY (barbearia_fk_endereco)
-  REFERENCES endereco (id_endereco)
-  ON DELETE SET NULL
-  ON UPDATE CASCADE
-);
-
 CREATE TABLE IF NOT EXISTS dia_semana (
-  id_dia_semana INT NOT NULL,
-  nome ENUM('Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo') NULL,
+  id_dia_semana INT NOT NULL AUTO_INCREMENT,
+  nome ENUM('SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB', 'DOM') NOT NULL,
   hora_abertura TIME NULL,
   hora_fechamento TIME NULL,
   ds_id_barbearia INT NOT NULL,
   PRIMARY KEY (id_dia_semana),
   INDEX fk_dia_semana_barbearia1_idx (ds_id_barbearia ASC) VISIBLE,
-  CONSTRAINT fk_dia_semana_barbearia1
+  CONSTRAINT Fk_dia_semana_barbearia1
     FOREIGN KEY (ds_id_barbearia)
     REFERENCES barbearia (id_barbearia)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS barbearia (
+  id_barbearia INT NOT NULL AUTO_INCREMENT,
+  nome_negocio VARCHAR(100) NULL,
+  email_negocio VARCHAR(100) NULL,
+  celular_negocio VARCHAR(15) NULL,
+  img_perfil BLOB NULL,
+  cnpj VARCHAR(18) NULL,
+  cpf VARCHAR(14) NULL,
+  descricao TEXT NULL,
+  barbearia_fk_endereco INT NOT NULL,
+  PRIMARY KEY (id_barbearia),
+  INDEX fk_barbearia_endereco1_idx (barbearia_fk_endereco ASC) VISIBLE,
+  CONSTRAINT fk_barbearia_endereco1
+    FOREIGN KEY (barbearia_fk_endereco)
+    REFERENCES endereco (id_endereco)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+    
+
+
 
 CREATE TABLE IF NOT EXISTS usuario (
   id_usuario INT NOT NULL AUTO_INCREMENT,
@@ -79,7 +85,7 @@ CREATE TABLE IF NOT EXISTS usuario (
 
     
 CREATE TABLE IF NOT EXISTS financeiro (
-  id_financeiro INT NOT NULL,
+  id_financeiro INT NOT NULL AUTO_INCREMENT,
   valor DECIMAL(8,2) NULL,
   dt_lancamento DATETIME NULL,
   financeiro_fk_barberia INT NOT NULL,
@@ -93,7 +99,7 @@ CREATE TABLE IF NOT EXISTS financeiro (
 );
     
 CREATE TABLE IF NOT EXISTS imgs_galeria (
-  id_imgs_galeria INT NOT NULL,
+  id_imgs_galeria INT NOT NULL AUTO_INCREMENT,
   imagem TEXT NULL,
   barbearia_id_barbearia INT NOT NULL,
   usuario_id_usuario INT NOT NULL,
@@ -113,7 +119,7 @@ CREATE TABLE IF NOT EXISTS imgs_galeria (
 );
 
 CREATE TABLE IF NOT EXISTS servico (
-  id_servico INT NOT NULL,
+  id_servico INT NOT NULL AUTO_INCREMENT,
   preco DECIMAL(7,2) NULL,
   descricao TEXT NULL,
   tipo_servico VARCHAR(180) NULL,
@@ -144,7 +150,7 @@ CREATE TABLE IF NOT EXISTS servico (
 );
 
 CREATE TABLE IF NOT EXISTS agendamento (
-  id_agendamento INT NOT NULL,
+  id_agendamento INT NOT NULL AUTO_INCREMENT,
   data_hora DATETIME NULL,
   concluido BOOLEAN NOT NULL,
   ag_fk_servico INT NOT NULL,
